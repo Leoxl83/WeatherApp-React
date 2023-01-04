0a765b48fc2596944814a4796724a922727133b5
Leoxl83 <leonardo.langorta@gmail.com>
2023-01-03 23:50:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import axios from 'axios';
+
 
 function App() {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=montevideo&appid={cb5afc911a9d809518289772f2de354e}`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <div className="container">
+        <div className="top">
+          <div className="ubicacion">
+            <p>Montevideo</p>
+          </div>
+          <div className="temperatura">
+            <h1>30°C</h1>
+          </div>
+          <div className="descripcion">
+            <p>Nublado</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="sensacion">
+            <p className="bold">35°C</p>
+            <p>Sensacion Termica</p>
+          </div>
+          <div className="humedad">
+            <p className="bold">80%</p>
+            <p>Humedad</p>
+          </div>
+          <div className="viento">
+            <p className="bold">30KMH</p>
+            <p>Viento</p>
+          </div>
+        </div>
+      </div>      
     </div>
   );
 }
